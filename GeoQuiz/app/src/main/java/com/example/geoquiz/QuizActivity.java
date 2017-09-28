d7a573f004f6458e044b0527c90ea6a41745689c
vedischevan <avedishchev@list.ru>
2017-09-28 18:45:21 +0400

    18.45 28.09

--- GeoQuiz/app/src/main/java/com/example/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/QuizActivity.java
@@ -1,14 +1,12 @@
 package com.example.geoquiz;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -24,7 +22,9 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {//слушатель
             @Override
             public void onClick(View v) {
-                // Пока ничего не делает, но скоро будет!
+                Toast.makeText(QuizActivity.this,  //информация для класса Toast, где искать идентификатор ресурса строки
+                        R.string.incorrect_toast,  //передается идентификатор ресурса строки
+                        Toast.LENGTH_SHORT).show();// время отображения сообщения
             }
         });
 
@@ -32,21 +32,23 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Пока ничего не делает, но скоро будет!
+                Toast.makeText(QuizActivity.this, //this означает View.OnClickListener
+                        R.string.correct_toast,
+                        Toast.LENGTH_SHORT).show();
             }
         });
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     @Override
