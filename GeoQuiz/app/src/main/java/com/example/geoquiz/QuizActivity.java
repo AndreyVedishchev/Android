ecbede71fe5c7fff314c6338dc5477d597c0b2f0
vedischevan <avedishchev@list.ru>
2017-09-29 18:44:25 +0400

    18.44 29.09

--- GeoQuiz/app/src/main/java/com/example/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/QuizActivity.java
@@ -6,17 +6,34 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+    private int mCurrentIndex = 0;
+
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+        int question = mQuestionBank[mCurrentIndex].getTextResID();
+        mQuestionTextView.setText(question);
 
         mTrueButton = (Button) findViewById(R.id.true_button);//Метод получает идентификатор ресурса виджета и возвращает объект View
         mTrueButton.setOnClickListener(new View.OnClickListener() {//слушатель
